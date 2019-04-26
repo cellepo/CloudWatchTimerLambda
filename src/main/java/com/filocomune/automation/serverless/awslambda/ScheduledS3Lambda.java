55ce5f408a41f8325351a8869d4f518daed5943d
Chris Ellepola <cellepo@gmail.com>
2019-04-25 19:49:14 -0700

    cloudwatch-timer-lambda - Append ScheduledEvent Rule ARN to timedExpirationKey.

--- src/main/java/com/filocomune/automation/serverless/awslambda/ScheduledS3Lambda.java
+++ src/main/java/com/filocomune/automation/serverless/awslambda/ScheduledS3Lambda.java
@@ -5,11 +5,10 @@ import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.filocomune.automation.serverless.awslambda.util.S3Util;
 
 /**
- * AWS Lambda triggered by CloudWatch Rule,
- *  that keys (by S3_KEY) current time String into S3_BUCKET_NAME if not already keyed there to a prior time.\n
+ * AWS Lambda triggered by CloudWatch Rule, that keys (by "expiration-<scheduledEventARN>") current time String
+ * into S3_BUCKET_NAME if not already keyed there to a prior time.\n
  * \n
  * Lambda Environment Variable "S3_BUCKET_NAME"\n
- * Lambda Environment Variable "S3_KEY"\n
  * \n
  * Action "logs:*" (in i.e: Policy "AWSLambdaExecute")\n
  * https://docs.aws.amazon.com/IAM/latest/UserGuide/list_amazons3.html\n
@@ -21,16 +20,18 @@ import com.filocomune.automation.serverless.awslambda.util.S3Util;
 public class ScheduledS3Lambda {
 
     protected final String s3BucketName = System.getenv("S3_BUCKET_NAME");
-    protected final String s3Key = System.getenv("S3_KEY");
 
     // https://docs.aws.amazon.com/lambda/latest/dg/with-scheduledevents-example.html
     public void handle(ScheduledEvent scheduledEvent) {
+        // https://docs.aws.amazon.com/lambda/latest/dg/with-scheduled-events.html
+        final String scheduledEventARN = scheduledEvent.getResources().get(0);
+        final String timedExpirationKey = "expiration-" + scheduledEventARN;
         try {
-            S3Util.getString(s3BucketName, s3Key);
+            S3Util.getString(s3BucketName, timedExpirationKey);
 
         } catch(AmazonS3Exception as3e){
             if(as3e.getStatusCode() == 404) {   // nothing already stored in S3 with timedExpirationKey
-                S3Util.putString(String.valueOf(System.currentTimeMillis()), s3BucketName, s3Key);
+                S3Util.putString(String.valueOf(System.currentTimeMillis()), s3BucketName, timedExpirationKey);
 
             } else {
                 throw as3e;
