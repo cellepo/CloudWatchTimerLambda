791f17426a105c34fb3bce48f280bc23100bf291
Chris Ellepola <cellepo@gmail.com>
2019-04-25 19:42:10 -0700

    cloudwatch-timer-lambda - Cleanup/Refactor ScheduledS3Lambda to new S3Util.

--- src/main/java/com/filocomune/automation/serverless/awslambda/ScheduledS3Lambda.java
+++ src/main/java/com/filocomune/automation/serverless/awslambda/ScheduledS3Lambda.java
@@ -1,13 +1,8 @@
 package com.filocomune.automation.serverless.awslambda;
 
 import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
-import com.amazonaws.services.s3.model.PutObjectResult;
-import com.amazonaws.services.s3.model.S3Object;
-
-import static com.filocomune.automation.serverless.awslambda.util.LambdaRuntimeUtil.log;
+import com.filocomune.automation.serverless.awslambda.util.S3Util;
 
 /**
  * AWS Lambda triggered by CloudWatch Rule,
@@ -28,24 +23,14 @@ public class ScheduledS3Lambda {
     protected final String s3BucketName = System.getenv("S3_BUCKET_NAME");
     protected final String s3Key = System.getenv("S3_KEY");
 
-    protected static final AmazonS3 amazonS3 = AmazonS3ClientBuilder.defaultClient();
-
     // https://docs.aws.amazon.com/lambda/latest/dg/with-scheduledevents-example.html
     public void handle(ScheduledEvent scheduledEvent) {
         try {
-            final S3Object s3Object = amazonS3.getObject(s3BucketName, s3Key);
-            if (s3Object != null) {
-                log("Retrieved S3Object {eTag: " + s3Object.getObjectMetadata().getETag() + "}");
-
-            } else {
-                log("null S3Object retrieved");
-            }
+            S3Util.getObject(s3BucketName, s3Key);
 
         } catch(AmazonS3Exception as3e){
             if(as3e.getStatusCode() == 404) {   // nothing already stored in S3 with timedExpirationKey
-                final PutObjectResult putObjectResult = amazonS3.putObject(
-                        s3BucketName, s3Key, String.valueOf(System.currentTimeMillis()));
-                log("Stored S3Object {eTag: " + putObjectResult.getETag() + "}");
+                S3Util.putString(String.valueOf(System.currentTimeMillis()), s3BucketName, s3Key);
 
             } else {
                 throw as3e;
---
+++ src/main/java/com/filocomune/automation/serverless/awslambda/util/S3Util.java
@@ -0,0 +1,61 @@
+package com.filocomune.automation.serverless.awslambda.util;
+
+import com.amazonaws.SdkClientException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.model.S3Object;
+
+import static com.filocomune.automation.serverless.awslambda.util.LambdaRuntimeUtil.log;
+
+/**
+ * Action "logs:*" (in i.e: Policy "AWSLambdaExecute")\n
+ * https://docs.aws.amazon.com/IAM/latest/UserGuide/list_amazons3.html\n
+ * Action "s3:PutObject" (in i.e: Policy "AWSLambdaExecute")\n
+ * Action "s3:List*" (in i.e: Policy "AmazonS3ReadOnlyAccess")\n
+ * Action "s3:Get*" (in i.e: Policy "AmazonS3ReadOnlyAccess")\n
+ */
+public class S3Util {
+
+    private static final AmazonS3 amazonS3 = AmazonS3ClientBuilder.defaultClient();
+
+    /**
+     * Action "logs:*" (in i.e: Policy "AWSLambdaExecute")\n
+     * Action "s3:PutObject" (in i.e: Policy "AWSLambdaExecute")\n
+     *
+     * @param string to put:  Becomes encoded UTF-8.
+     * @param bucketName
+     * @param key
+     * @return {@link PutObjectResult}
+     * @throws SdkClientException
+     */
+    public static PutObjectResult putString(String string, String bucketName, String key) throws SdkClientException {
+        final PutObjectResult putObjectResult = amazonS3.putObject(bucketName, key, string);
+        log("Stored S3Object {eTag: " + putObjectResult.getETag() + "}");
+
+        return putObjectResult;
+    }
+
+    /**
+     * Action "logs:*" (in i.e: Policy "AWSLambdaExecute")\n
+     * Action "s3:List*" (in i.e: Policy "AmazonS3ReadOnlyAccess")\n
+     * Action "s3:Get*" (in i.e: Policy "AmazonS3ReadOnlyAccess")\n
+     *
+     * @param bucketName
+     * @param key
+     * @return {@link S3Object}
+     * @throws SdkClientException
+     */
+    public static S3Object getObject(String bucketName, String key) throws SdkClientException {
+        final S3Object s3Object = amazonS3.getObject(bucketName, key);
+        if (s3Object != null) {
+            log("Retrieved S3Object {eTag: " + s3Object.getObjectMetadata().getETag() + "}");
+
+        } else {
+            log("null S3Object retrieved");
+        }
+
+        return s3Object;
+    }
+
+}
