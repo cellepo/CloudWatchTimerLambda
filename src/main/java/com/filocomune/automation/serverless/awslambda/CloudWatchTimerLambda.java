3667aa2206e3155cf27ac5ea5823617346c24c88
Chris Ellepola <cellepo@gmail.com>
2019-04-25 19:49:46 -0700

    cloudwatch-timer-lambda - Convert ScheduledS3Lambda -> CloudWatchTimerLambda: disableRule after expired.

---
+++ src/main/java/com/filocomune/automation/serverless/awslambda/CloudWatchTimerLambda.java
@@ -0,0 +1,67 @@
+package com.filocomune.automation.serverless.awslambda;
+
+import com.amazonaws.services.cloudwatchevents.AmazonCloudWatchEvents;
+import com.amazonaws.services.cloudwatchevents.AmazonCloudWatchEventsClientBuilder;
+import com.amazonaws.services.cloudwatchevents.model.DisableRuleRequest;
+import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.filocomune.automation.serverless.awslambda.util.S3Util;
+
+import static com.filocomune.automation.serverless.awslambda.util.LambdaRuntimeUtil.log;
+
+/**
+ * AWS Lambda triggered by CloudWatch Rule, that keys (by "expiration-<scheduledEventARN>") current time String
+ * into S3_BUCKET_NAME if not already keyed there to a prior time, otherwise disables the CloudWatch Rule.\n
+ * \n
+ * Lambda Environment Variable "S3_BUCKET_NAME"\n
+ * \n
+ * Action "logs:*" (in i.e: Policy "AWSLambdaExecute")\n
+ * https://docs.aws.amazon.com/IAM/latest/UserGuide/list_amazons3.html\n
+ * Action "s3:List*" (in i.e: Policy "AmazonS3ReadOnlyAccess")\n
+ * Action "s3:Get*" (in i.e: Policy "AmazonS3ReadOnlyAccess")\n
+ * Action "s3:PutObject" (in i.e: Policy "AWSLambdaExecute")\n
+ * https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/permissions-reference-cwe.html\n
+ * Action "events:DisableRule"\n
+ */
+// https://blog.symphonia.io/learning-lambda-1f25af64161c
+public class CloudWatchTimerLambda {
+
+    protected final String s3BucketName = System.getenv("S3_BUCKET_NAME");
+
+    private static final AmazonCloudWatchEvents amazonCloudWatchEvents
+            = AmazonCloudWatchEventsClientBuilder.defaultClient();
+
+    // https://docs.aws.amazon.com/lambda/latest/dg/with-scheduledevents-example.html
+    public void handle(ScheduledEvent scheduledEvent) {
+        // https://docs.aws.amazon.com/lambda/latest/dg/with-scheduled-events.html
+        final String scheduledEventARN = scheduledEvent.getResources().get(0);
+        final String timedExpirationKey = "expiration-" + scheduledEventARN;
+        try {
+            final long scheduledEventRuleExpirationMillis
+                    = Long.parseLong(S3Util.getString(s3BucketName, timedExpirationKey));
+            if(scheduledEventRuleExpirationMillis < System.currentTimeMillis()){
+                // https://docs.aws.amazon.com/lambda/latest/dg/with-scheduled-events.html
+                final String cloudWatchRuleNameARNPrefix = ":rule/";
+                final String cloudWatchRuleName = scheduledEventARN.substring(
+                        scheduledEventARN.indexOf(cloudWatchRuleNameARNPrefix) + cloudWatchRuleNameARNPrefix.length());
+
+                amazonCloudWatchEvents.disableRule(new DisableRuleRequest().withName(cloudWatchRuleName));
+                log(scheduledEventARN + " disabled\n" +
+                        "  (expired at " + scheduledEventRuleExpirationMillis + ")");
+
+            } else {
+                log(timedExpirationKey + " is " + scheduledEventRuleExpirationMillis);
+            }
+
+        } catch(AmazonS3Exception as3e){
+            if(as3e.getStatusCode() == 404) {   // nothing already stored in S3 with timedExpirationKey
+                S3Util.putString(String.valueOf(System.currentTimeMillis()), s3BucketName, timedExpirationKey);
+
+            } else {
+                throw as3e;
+            }
+        }
+    }
+
+}
+
